dc1dfa03402d95a43842a7e0cafa5a3ba51f1f2b
mickalala <mickalarissa01@gmail.com>
2023-04-17 16:16:32 -0300

    Recusando mensagem com type errado

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ app.post("/messages", (req, res) => {
     const messageSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
-        type: joi.string().required()
+        type: joi.any().valid('message', 'private_message')
     })
 
     const messageValid = messageSchema.validate(req.body)
