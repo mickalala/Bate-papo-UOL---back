6afbae79ffada69dfb961161cf5601f92bd04c22
mickalala <mickalarissa01@gmail.com>
2023-04-17 17:35:07 -0300

    Adicionando o type como required

--- src/app.js
+++ src/app.js
@@ -54,11 +54,14 @@ app.get("/participants", (req, res) => {
 app.post("/messages", (req, res) => {
     const { to, text, type } = req.body
     const user = req.headers.user
+    const seeList =  db.collection("participants").findOne({name:user})
+
+   if(!user || !seeList) return res.sendStatus(422)
 
     const messageSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
-        type: joi.any().valid('message', 'private_message')
+        type: joi.any().valid('message', 'private_message').required
     })
 
     const messageValid = messageSchema.validate(req.body)
