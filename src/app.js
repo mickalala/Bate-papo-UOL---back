b193e72526a321059ae99f0a016711ae0abe75a4
mickalala <mickalarissa01@gmail.com>
2023-04-17 12:13:38 -0300

    Arrumando a data da mensagem e o nome na coleção de participantes

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ let db
 const nameSchema = joi.object({
     name: joi.string().required()
 })
-const messageSchema= joi.object({
+const messageSchema = joi.object({
     to: joi.string().required(),
     text: joi.string().required(),
     type: joi.string().required()
@@ -26,32 +26,35 @@ const messageSchema= joi.object({
 mongoClient.connect().then(() => db = mongoClient.db())
 
 app.post("/participants", (req, res) => {
-    const { name } = req.body
+    const { nameBody } = req.body
     const validate = nameSchema.validate(req.body)
     if (validate.error) return res.sendStatus(422)
-  
-    db.collection("participantes").insertOne({
-        name: name,
+
+
+
+    console.log(dayjs().format('HH:mm:ss'))
+    db.collection("participants").insertOne({
+        name: nameBody,
         lastStatus: Date.now()
     }).then(() => res.sendStatus(201))
         .catch(() => res.sendStatus(500))
     db.collection("messages").insertOne({
-        from: name,
-		to: 'Todos',
-		text: 'entra na sala...',
-		type: 'status',
-		time: dayjs()
+        from: nameBody,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs().format('HH:mm:ss')
     }).then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500))
+        .catch(() => res.sendStatus(500))
 })
 
-app.get("/participants",(req,res)=>{
-    db.collection("participantes").find().toArray()
-    .then(partics=>res.send(partics))
-    .catch(() => res.sendStatus(500))
+app.get("/participants", (req, res) => {
+    db.collection("participants").find().toArray()
+        .then(partics => res.send(partics))
+        .catch(() => res.sendStatus(500))
 })
 
-app.post("/messages",(req,res)=>{
+app.post("/messages", (req, res) => {
 
 })
 const PORT = 5000;
